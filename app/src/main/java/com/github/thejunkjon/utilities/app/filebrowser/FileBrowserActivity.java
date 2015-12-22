848f8ca2a80fc7a33fd71cb67a347aa44a1836f3
thejunkjon <thejunkjon@gmail.com>
2015-12-22 01:06:56 -0800

    adding very simple file browser functionality that only includes listing files and directories.

--- app/src/main/java/com/github/thejunkjon/utilities/app/filebrowser/FileBrowserActivity.java
+++ app/src/main/java/com/github/thejunkjon/utilities/app/filebrowser/FileBrowserActivity.java
@@ -1,29 +1,70 @@
 package com.github.thejunkjon.utilities.app.filebrowser;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.github.thejunkjon.utilities.app.R;
-import com.github.thejunkjon.utilities.app.TitlelessActivity;
 
-public class FileBrowserActivity extends TitlelessActivity {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.sort;
+
+public class FileBrowserActivity extends ListActivity {
+
+    private String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_browser);
+
+        path = "/";
+        if (getIntent().hasExtra("path")) {
+            path = getIntent().getStringExtra("path");
+        }
+        setTitle(path);
+
+        final List<String> values = new ArrayList<>();
+        final File dir = new File(path);
+        if (!dir.canRead()) {
+            setTitle(getTitle() + " (inaccessible)");
+        }
+        final String[] list = dir.list();
+        if (list != null) {
+            for (String file : list) {
+                if (!file.startsWith(".")) {
+                    values.add(file);
+                }
+            }
+        }
+        sort(values);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_2, android.R.id.text1, values);
+        setListAdapter(adapter);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.file_browser, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String filename = (String) getListAdapter().getItem(position);
+        if (path.endsWith(File.separator)) {
+            filename = path + filename;
+        } else {
+            filename = path + File.separator + filename;
+        }
+        if (new File(filename).isDirectory()) {
+            Intent intent = new Intent(this, FileBrowserActivity.class);
+            intent.putExtra("path", filename);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, filename + " is not a directory", Toast.LENGTH_LONG).show();
+        }
     }
 }
